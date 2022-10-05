c985fd523f92522850811a21983a7fdf58d2404f
ldhwaddell <ldhwaddell@gmail.com>
2022-10-05 11:39:56 -0300

    update README, add start command to package.json, start index.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CSCI-3428-Pictionary
 Source code for a Mi'kmaq Pictionary Game. 
+
+# HTML
+
+# CSS
+
+# JS
+The index.js file can be ran and automatically restarded should a file be changed with the command "npm run dev"
---
+++ js/index.js
@@ -0,0 +1,10 @@
+const path = require("path"); // allow access to env
+const express = require("express");
+const app = express(); //instantiate express app
+const port = process.env.PORT || 3000; //port 3000 or one defined in env
+
+app.use(express.static("public/assets")); // expose the directory public/assets
+
+app.listen(port, () => {
+  console.log("Listening at http://localhost:${port}");
+});
