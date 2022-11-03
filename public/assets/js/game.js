6e8d812690e21629f7b45e80861ea705dded19fe
ldhwaddell <ldhwaddell@gmail.com>
2022-11-03 16:07:47 -0300

    add start of game logic

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,10 +1,8 @@
 //file for the game logic
 const playButton = document.getElementById("playButton");
 
-// list all files in the directory
-
 // Global array containing 9 objects containing image files.
-const questions = [
+const allQuestions = [
   { id: "0", name: "aqq", img: "aqq.jpg", audio: "aqq.wav" },
   { id: "1", name: "eliey", img: "eliey.jpg", audio: "eliey.wav" },
   { id: "2", name: "kesalk", img: "kesalk.jpg", audio: "kesalk.wav" },
@@ -14,134 +12,22 @@ const questions = [
   { id: "6", name: "nin", img: "nin.jpg", audio: "nin.wav" },
   { id: "7", name: "teluisi", img: "teluisi.jpg", audio: "teluisi.wav" },
   { id: "8", name: "wiktm", img: "wiktm.jpg", audio: "wiktm.wav" },
-  ,
 ];
 
-// The purpose of this function is to generate the default layout of the page.
-//      Author: Olly MacDonald
+//make a copy of all the questions to manipulate freely.
+//This way, when user beats the game, we can reset the array contaiing all the questions
+//and do not need to worry about destructive manipulation.
+var remainingQuestions = JSON.parse(JSON.stringify(allQuestions));
 
-function setup() {
-  // Generate a value that corresponds to an element in the words array
-  // In the future this should be able to skip empty cells/words marked
-  // as inactive, but for now the array is static so we just pull from it.
-  var currentCorrectID = getRandomInt(0, 8);
-
-  // Generate a value that corresponds to one of three spaces for images
-  var currentCorrectSpc = getRandomInt(1, 3);
-
-  // Generate IDs for incorrect answers.
-  // Loop until unused ID found for incorrect answer 1
-  do {
-    var incorrectID1 = getRandomInt(0, 8);
-  } while (incorrectID1 === currentCorrectID);
-
-  // Loop until unused ID found for incorrect answer 2
-  do {
-    var incorrectID2 = getRandomInt(0, 8);
-  } while (incorrectID2 === currentCorrectID || incorrectID2 === incorrectID1);
-
-  // Generate space val 1 or 2 for incorrect answer 1.
-  // Incorrect answer 2 will get the other.
-  var incorrectSpc1 = getRandomInt(1, 2);
-
-  // Display word associated with current correct answer
-  document.getElementById("activeWord").innerHTML =
-    questions[currentCorrectID].name;
-
-  // Display images. Code derived from example provided by Terrence Goldsmith
-  // To be added: padding
-  let str1 = '<input class="picButton" type="image" src="./images/';
-  let str2 = '" width="325" height="325" onclick="choose(corr)"/>';
-  let str3 = str1 + questions[currentCorrectID].img + str2;
-
-  // If correct answer is displayed in space 1, the others will be displayed in 2 and 3
-  if (currentCorrectSpc === 1) {
-    document.getElementById("pic1").innerHTML = str3;
-
-    // If incorrect answer 1 is in space 2, incorrect answer 2 will be displayed in space 3
-    // If incorrect answer 1 is in space 3, incorrect answer 2 will be displayed in space 2
-    if (incorrectSpc1 === 1) {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic2").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic3").innerHTML = str3;
-    } else {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic3").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic2").innerHTML = str3;
-    }
-  }
-
-  if (currentCorrectSpc === 2) {
-    document.getElementById("pic2").innerHTML = str3;
-
-    if (incorrectSpc1 === 1) {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic1").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic3").innerHTML = str3;
-    } else {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic3").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic1").innerHTML = str3;
-    }
-  }
-
-  if (currentCorrectSpc === 3) {
-    document.getElementById("pic3").innerHTML = str3;
-
-    if (incorrectSpc1 === 1) {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic1").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic2").innerHTML = str3;
-    } else {
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID1].img + str2;
-      document.getElementById("pic2").innerHTML = str3;
-
-      str2 = '" width="325" height="325" onclick="choose(incorr)"/>';
-      str3 = str1 + questions[incorrectID2].img + str2;
-      document.getElementById("pic1").innerHTML = str3;
-    }
-  }
-}
-
-var allQuestions = [
-  { id: "0", name: "aqq", img: "aqq.jpg", audio: "aqq.wav" },
-  { id: "1", name: "eliey", img: "eliey.jpg", audio: "eliey.wav" },
-  { id: "2", name: "kesalk", img: "kesalk.jpg", audio: "kesalk.wav" },
-  { id: "3", name: "kil", img: "kil.jpg", audio: "kil.wav" },
-  { id: "4", name: "ltu", img: "ltu.jpg", audio: "ltu.wav" },
-  { id: "5", name: "mijisi", img: "mijisi.jpg", audio: "mijisi.wav" },
-  { id: "6", name: "nin", img: "nin.jpg", audio: "nin.wav" },
-  { id: "7", name: "teluisi", img: "teluisi.jpg", audio: "teluisi.wav" },
-  { id: "8", name: "wiktm", img: "wiktm.jpg", audio: "wiktm.wav" },
-  ,
-];
-
+//variable instantiation. Plurals are lists.
 var correctAnswers = [];
+//variable to hod current correct answer (changes each round)
 var correctAnswer;
+//variavle to hold the list containing the current questions being asked each round.
+var currentQuestions;
 
 // Function for generating ints within a range
-//     Written by Olly
+//     Author: Olly Macdonald
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -181,42 +67,137 @@ function playSound() {
   // get activeWord container
   const activeWord = document.getElementById("activeWord").textContent;
   //find the value for the audio key where the elements name is the active word
-  var audioName = questions.find((element) => element.name == activeWord).audio;
+  var audioName = remainingQuestions.find(
+    (element) => element.name == activeWord
+  ).audio;
+  //play the audio
   let audio = new Audio(`./audioFiles/${audioName}`);
   audio.play();
 }
 
+/*
+  The purpose of this function is to load the questions for the user. 
+  It is first loaded at runtime and then gets called again should the user pick the correct answer. 
+
+  Authors: Olly Macdonald, Lucas Waddell
+*/
 function loadQuestions() {
-  // pick three questions from the list of all questions.
-  var currentQuestions = getRandoms(allQuestions, 3);
-  //console.log(currentQuestions);
-  correctAnswer = currentQuestions[getRandomInt(0, 2)];
-  //console.log(correctAnswer);
+  //if the user got all answers right, call beatTheGame
+  if (correctAnswers.length == 9) {
+    beatTheGame();
+  }
+
+  //If there are less than three questions left
+  if (remainingQuestions.length < 3 && correctAnswers.length < 9) {
+    //console.log(JSON.stringify(correctAnswers));
+    //pick a correct answer from the remaining questions
+
+    //pick a correct answer from the remaining questions
+    correctAnswer =
+      remainingQuestions[getRandomInt(0, remainingQuestions.length - 1)];
+    //console.log(`correct answer is: ${JSON.stringify(correctAnswer)}`)
+
+    //DEtermine how many questions will need to be added to the current remaining ones
+    var numQuestionsToAdd = 3 - remainingQuestions.length;
+    //console.log(`questions to add: ${numQuestionsToAdd}`);
+
+    //pick however many questions at random from ones user has already gotten correct
+    var questionsToAdd = getRandoms(correctAnswers, numQuestionsToAdd);
+    //console.log(`questions to add from correct ones are: ${JSON.stringify(questionsToAdd)}`);
+
+    //push those questions onto the array of remaining questions
+    remainingQuestions.push(questionsToAdd[0]);
+
+    //console.log(`remaining questions are now: ${JSON.stringify(remainingQuestions)}`);
+
+    //set the current questions equal to the remaining questions
+    currentQuestions = remainingQuestions;
+  } else {
+    // pick three questions from the list of all questions.
+    currentQuestions = getRandoms(remainingQuestions, 3);
+    //pick one to bethe right answer
+    correctAnswer = currentQuestions[getRandomInt(0, 2)];
+  }
 
   // Display word associated with current correct answer
   document.getElementById("activeWord").innerHTML = correctAnswer.name;
 
   //randomize order of current questions;
+  //This is an application of the "Schwartzian transform". The logic behind
+  //the transform can be found at : "https://en.wikipedia.org/wiki/Schwartzian_transform"
+  //The source for this code was: "https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array"
   let randomOrderedQuestions = currentQuestions
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
 
+  // logic for insertion of the three chosen pictures into the game
   let params = '<input class="picButton" type="image" src="./images/';
-  var pic = 1;
+  var picNum = 1;
+  //iterate through the randomly ordered list of questions
   randomOrderedQuestions.forEach((question) => {
     let dimensions = `" width="325" height="325" onclick="choose('${question.name}')""/>`;
-    document.getElementById(`pic${pic}`).innerHTML =
+    document.getElementById(`pic${picNum}`).innerHTML =
       params + question.img + dimensions;
-    pic += 1;
+    picNum += 1;
   });
 }
 
-function choose(selection) {
-  console.log(selection);
-  console.log(correctAnswer);
+/*
+  Function to remove a desired JSON object from a JSON array.
+  User supplies the array to remove and the name of the element to remove
+*/
+function removeElement(arr, answer) {
+  const indexToRemove = arr.findIndex((element) => element.name === answer);
+  if (indexToRemove !== undefined) {
+    arr.splice(indexToRemove, 1);
+  }
 }
 
-// adding the event listeners to all buttons
+/*
+  Function called each time the user clicks on a picture. 
+  If the selection is correct, we push the correct answer onto
+  the list of questions they have gotten correctly, then we remove
+  this question from the list of remaining questions to that it will 
+  not be asked again. 
+*/
+function choose(selection) {
+  if (selection == correctAnswer.name) {
+    //add correct answer to correctAnswers:
+    correctAnswers.push(correctAnswer);
+
+    //remove question from all questions
+    removeElement(remainingQuestions, selection);
+
+    //let user know they got it right
+    //instead of alerts these could be function calls to create modals
+    alert("Congratulations!!");
+
+    // reload the questions
+    loadQuestions();
+  } else {
+    //Could maybe put the logic here to deal with prioritizing words
+
+    //instead of alerts these could be function calls to create modals
+    alert("Please Try Again");
+  }
+}
+
+/*
+  Display alert once user beats the game (list of correct answer has length of 9)
+  This should probably use a modal that has buttons for yes or no. Could also display 
+  some fun graphics or play a congratulatory noise or something idk 
+*/
+function beatTheGame() {
+  alert("Woo hoo!! you won the game. Click ok to restart or close this page");
+  //reset the correct answer list and restart game
+  correctAnswers.length = 0;
+  //add all questions back to remaining questions
+  remainingQuestions = JSON.parse(JSON.stringify(allQuestions));
+
+  //reload questions
+  loadQuestions();
+}
+
+// adding the event listener to playSound button
 playButton.addEventListener("click", playSound, false);
-//let dimensions = `" width="325" height="325" onclick="choose('${question.name}', ${JSON.stringify(correctAnswer)})"/>`;
