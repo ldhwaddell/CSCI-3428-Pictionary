9ce28cf95c8a19157b9584da2da9683fa249f44c
ldhwaddell <ldhwaddell@gmail.com>
2022-12-01 10:14:07 -0400

    Add stylized comments to game.js

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -3,18 +3,29 @@
   game. It randomizes the selection of questions, displays them, and deals with
   guessing. 
 
-  Authors: 
+  Authors: Clifford Brown, Lucas Waddell, Sebastion, Olly MacDonald
+
+  Work Done: Olly MacDonald focused on displaying the questions
+  Clifford Brown and Sebastian Cox focusd on making guesses and creating game logic
+  Nickieda Johnson and Lucas Waddell focused on logic for playing sounds
  */
 
+// ------------------------------Global constants and Variables------------------------------
+/* The server "Socket" used to connect.
+ * The Internet Protocol is 140.184.230.209 as
+ * specified in the Software Requirements
+ * and the Transport Layer Protocol is 3000.
+ * The http server is not part of the Secure Socket Layer.
+ */
 const SERVER_URL = "http://140.184.230.209:3000";
 
-// Getting necessary HTML element
+// Getting playbutton, settingsbutton, success, and mistake image HTML elements
 const playButton = document.getElementById("playButton");
 const settingsButton = document.getElementById("settingsButton");
 const successImage = document.getElementById("successImage");
 const mistakeImage = document.getElementById("mistakeImage");
 
-// Global array containing 9 objects containing image files.
+// Global array containing an object for each question.
 // This is never manipulated and serves as the comprehensive list
 // of questions
 const allQuestions = [
@@ -34,15 +45,22 @@ const allQuestions = [
 //and do not need to worry about destructive manipulation.
 var remainingQuestions = JSON.parse(JSON.stringify(allQuestions));
 
-//variable instantiation. Plurals are lists.
+//Variable instantiation. Plurals are lists.
 var correctAnswers = [];
-//variable to hold current correct answer (changes each round)
+//Variable to hold current correct answer (changes each round)
 var correctAnswer;
-//variable to hold the list containing the current questions being asked each round.
+//Variable to hold the list containing the current questions being asked each round.
 var currentQuestions;
 
+// ------------------------------Functions------------------------------
+
 /*
-  The purpose of this function is to generate random integers within a range
+  Purpose: The purpose of this function is to generate random integers within a range
+
+  Parameters: (1) min: The minimum number in range of randoms to generate
+              (2) max: The maximum number in range of randoms to generate
+
+  Return: A random single integer in the given range
 
   Author: Olly Macdonald
 */
@@ -51,23 +69,28 @@ function getRandomInt(min, max) {
 }
 
 /*
-  The purpose of this function is to get n random elements from an array without having
-  any destructive effects on the original array.
-  The ... (spread syntax) is used to create a copy of the array that we
-  want to pick elements from. The sort function gets passed an arrow function that 
-  serves as a comparison function. 
+  Purpose: The purpose of this function is to get n random elements from an array without having
+        any destructive effects on the original array.
+        The ... (spread syntax) is used to create a copy of the array that we
+        want to pick elements from. The sort function gets passed an arrow function that 
+        serves as a comparison function. 
 
-  The sort function always takes in two items from the array. Math.random 
-  returns a random number between 0 and 1. 0.5 is used to that the values that
-  math.random returns can now also be negative (0.3424 - 0.5 < 0). If sort gets
-  a value greater than 0, it sorts the second element before the first. 
-  If the value is less than 0, it sorts the first element before the second. 
+        The sort function always takes in two items from the array. Math.random 
+        returns a random number between 0 and 1. 0.5 is used to that the values that
+        math.random returns can now also be negative (0.3424 - 0.5 < 0). If sort gets
+        a value greater than 0, it sorts the second element before the first. 
+        If the value is less than 0, it sorts the first element before the second. 
 
-  Slice is then used to make a copy of the randomized array from the 0th index
-  to the nth index that we desire, and returns it. 
+        Slice is then used to make a copy of the randomized array from the 0th index
+        to the nth index that we desire, and returns it. 
 
-  This is not purely proprietary code. It is a common problem and 
-  many, many examples exist on the internet. 
+        This is not purely proprietary code. It is a common problem and 
+        many, many examples exist on the internet. 
+
+  Parameters: (1) questions: The array to pick random questions from 
+              (2) n: The number of random elements to return
+
+  Return: An array of n random elements
 
   Author: Lucas Waddell
  */
@@ -78,20 +101,32 @@ function getRandoms(questions, n) {
 }
 
 /*
-  The purpose of this function of the find the desired element in
-  the array of remaining questions based on a value. It searches for the element 
-  whos name is the desired value, then returns the value of the user specified key.
+  Purpose: The purpose of this function of the find the desired element in
+          the array of remaining questions based on a value. It searches for the element 
+          whos name is the desired value, then returns the value of the user specified key.
+
+  Parameters: (1) key: The key to return from the element that has the desired name value
+              (2) val: The val of the name field in the array of remaining questions to look for
+
+  Returns: The element that was found
+
+  Authors: Clifford Brown
  */
 function findElement(key, val) {
   return remainingQuestions.find((element) => element.name == val)[key];
 }
 
 /*
-  The purpose of this function is to play the audio clip for the current word.
-  It checks if the audio clip is just a reference to a .wav file, or if it is an
-  audioURL that the user has created.
+  Purpose: The purpose of this function is to play the audio clip for the current word.
+          It checks if the audio clip is a reference to a .wav file, or if it is an
+          audioURL that the user has created. Based on that, it will play the audio clip by either 
+          loading the .wav file or creating a new audio object from the audio URL.
 
-  Author: Lucas Waddell
+  Parameters: None
+
+  Return: Plays audio if successful, logs error if failed
+
+  Author: Lucas Waddell, Nickieda Johnson
 */
 function playSound() {
   // get activeWord container
@@ -110,18 +145,22 @@ function playSound() {
 }
 
 /*
-  The purpose of this function is to load the questions for the user. 
-  It is first loaded at runtime and then gets called again should the 
-  user pick the correct answer. It checks if the user got all the questions right, and
-  will reuse pictures that have already been guessed correctly when the user only has 1 or 2
-  questions left. 
+  Purpose: The purpose of this function is to load the questions for the user. 
+            It is first loaded at runtime and then gets called again should the 
+            user pick the correct answer. It checks if the user got all the questions right, and
+            will reuse pictures that have already been guessed correctly when the user only has 1 or 2
+            questions left. 
 
-  To load the questions, the function picks 3 random questions from the users remaiming
-  questions. It then picks a random integer between 0 and 2, and assigns that element to be
-  the correct answer for this round. The middle of the screen then displays the name of the 
-  correct answer. These three questions then get put in a random order. This ensure that 
-  the correct question will not always be in the same box. Lastly, the three questions get
-  injected into the html.
+            To load the questions, the function picks 3 random questions from the users remaiming
+            questions. It then picks a random integer between 0 and 2, and assigns that element to be
+            the correct answer for this round. The middle of the screen then displays the name of the 
+            correct answer. These three questions then get put in a random order. This ensure that 
+            the correct question will not always be in the same box. Lastly, the three questions get
+            injected into the html.
+
+  Parameters: None
+
+  Return: None
 
   Authors: Olly Macdonald, Lucas Waddell
 */
@@ -180,11 +219,16 @@ function loadQuestions() {
 }
 
 /*
-  The purpose of this function is to removed a desired JSON object from a JSON array.
-  The index of the element to be removed is found and if that value is not undefined, 
-  it gets spliced out of the array. 
+  Purpose: The purpose of this function is to removed a desired JSON object from a JSON array.
+          The index of the element to be removed is found and if that value is not undefined, 
+          it gets spliced out of the array. 
 
-  Author: 
+  Parameters: (1) arr: The array to remove the element from
+              (2) index: The name of the element to be removed 
+
+  Returns: None
+
+  Author: Sebastian Cox, Olly Macdonald
 */
 function removeElement(arr, answer) {
   const indexToRemove = arr.findIndex((element) => element.name === answer);
@@ -194,11 +238,16 @@ function removeElement(arr, answer) {
 }
 
 /*
-  The purpose of this function is to allow the user to make guesses. 
-  If the user makes a correct guess, the questions is removed from the list of 
-  remaining questions and added to the list of answers they have gotten correct. 
+  Purpose: The purpose of this function is to allow the user to make guesses. 
+          If the user makes a correct guess, the questions is removed from the list of 
+          remaining questions and added to the list of answers they have gotten correct. A picture is shown
+          on screen that correlates to them making a correct or incorrect guess
+  
+  Parameters: (1) selection: The selection the user made 
 
-  Author: Sebastian Cox, Lucas Waddell
+  Returns: None
+
+  Author: Sebastian Cox, Clifford Brown
 */
 function choose(selection) {
   if (selection == correctAnswer.name) {
@@ -221,11 +270,15 @@ function choose(selection) {
 }
 
 /*
-  The purpose of this function is to let the user know they have gotten
-  every question correct. Once this happens, it empties their list of correct answers, 
-  resets the array of remaining questions they have, and sends the user back to the title page. 
+  Purpose: The purpose of this function is to let the user know they have gotten
+          every question correct. Once this happens, it empties their list of correct answers, 
+          resets the array of remaining questions they have, and sends the user back to the title page. 
+  
+  Parameters: None
 
-  Author: Clifford Brown, Lucas Waddell
+  Returns: None
+
+  Author: Clifford Brown, Sebastian Cox
 */
 function beatTheGame() {
   alert("Woo hoo!! you won the game. Click ok to restart or close this page");
@@ -242,8 +295,12 @@ function beatTheGame() {
 }
 
 /*
-  The purpose of this function is to hide any element that gets
-  passed to it. 
+  Purpose: The purpose of this function is to hide any element that gets
+           passed to it by changing its CSS properties
+
+  Parameters: (1): The element to be hidden
+
+  Returns: None
 
   Author: Lucas Waddell
  */
@@ -256,6 +313,10 @@ function hideElement(element) {
   }
 }
 
+/*
+  Purpose: The purpose of this function is to simply test the connection to the server. 
+  Since this game does not require the server, this method is not really important.
+*/
 function settings() {
   console.log("settings");
   let obj = {
@@ -265,8 +326,15 @@ function settings() {
 }
 
 /*
-  Wrapper for the hide element function to add into the correct event listener.
-  This way when user clicks the animal, it will disappear along with the div
+  Purpose: The purpose of this function is to test the connection to the server.
+    Wrapper for the hide element function to add into the correct event listener.
+    This way when user clicks the animal, it will disappear along with the div
+
+  Parameters: None
+
+  Returns: None
+
+  Author: Nickieda Johnson
  */
 function hideSuccess() {
   hideElement("successImage");
@@ -275,18 +343,23 @@ function hideSuccess() {
 }
 
 /*
-  Wrapper for the hide element function to add into the event mistake listener.
+  Purpose: Wrapper for the hide element function to add into the event mistake listener.
   This way when user clicks the animal, it will disappear along with the div
+
+  Parameters: None
+
+  Returns: None
+
+  Author: Nickieda Johnson
  */
 function hideMistake() {
   hideElement("mistakeImage");
   hideElement("incorrectAnswer");
 }
 
-// adding the event listener to playSound button
+// ------------------------------Binding Buttons to Function Clicks------------------------------
+
 playButton.addEventListener("click", playSound, false);
 settingsButton.addEventListener("click", settings, false);
 successImage.addEventListener("click", hideSuccess, false);
 mistakeImage.addEventListener("click", hideMistake, false);
-
-//https://ugdev.cs.smu.ca/~group23C/CSCI-3428-Pictionary/
